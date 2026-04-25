fd50c34bbef1fde9235006298c99a93377623907
kodjirorengoku-create <kodjirorengoku@gmail.com>
2026-04-25 23:49:53 +0500

    Create while_loop_name.py

---
+++ while_loop_name.py
@@ -0,0 +1,7 @@
+name = ""
+while name != "Islam":
+    name = input("Введите скрытое имя: ")
+    if name == "Islam":
+        print("Приветстыую вас", name, sep=" ")
+    else:
+        print("Введите имя пользователя занова: ")
