940510d24ea3c690a1800edc079859fe099a8701
kodjirorengoku-create <kodjirorengoku@gmail.com>
2026-05-09 21:48:50 +0500

    Delete simple_auth.py

--- simple_auth.py
+++
@@ -1,7 +0,0 @@
-  # Вход с помощью логина и пороля.
-login = input("Введите имя: ").strip()
-password = input("Введите пароль: ")
-if login == "admin" and password == "1234":
-    print("Вход разрешен")
-else:
-    print("Ошибка доступа")
