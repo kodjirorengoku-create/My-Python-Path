663470f81367ad1b402e408cf6ff8f1f021d09ab
kodjirorengoku-create <kodjirorengoku@gmail.com>
2026-05-09 21:48:27 +0500

    Delete while_loop_name.py

--- while_loop_name.py
+++
@@ -1,7 +0,0 @@
-name = ""
-while name != "Islam":
-    name = input("Введите скрытое имя: ")
-    if name == "Islam":
-        print("Приветстыую вас", name, sep=" ")
-    else:
-        print("Введите имя пользователя занова: ")
